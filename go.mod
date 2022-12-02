06d801311e01dafc18406ad40e1328941f080f11
sachindra-maharjan <sachindra.maharjan@outlook.com>
2022-12-02 22:39:39 +0000

    added docker for building package; added unit tests

--- go.mod
+++ go.mod
@@ -5,14 +5,17 @@ go 1.19
 require (
 	github.com/Masterminds/squirrel v1.5.3
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/stretchr/testify v1.2.2
 	google.golang.org/grpc v1.51.0
 	google.golang.org/protobuf v1.28.1
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/text v0.4.0 // indirect
---
+++ internal/adapters/core/arithmetic/arithmetic_test.go
@@ -0,0 +1,50 @@
+package arithmetic
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestAddition(t *testing.T) {
+	arith := NewAdapter()
+	answer, err := arith.Addition(1, 1)
+	if err != nil {
+		t.Fatalf("expected: %v, got: %v", nil, err)
+	}
+
+	require.Equal(t, answer, int32(2))
+
+}
+
+func TestSubstraction(t *testing.T) {
+	arith := NewAdapter()
+	answer, err := arith.Substraction(2, 1)
+	if err != nil {
+		t.Fatalf("expected: %v, got: %v", nil, err)
+	}
+
+	require.Equal(t, answer, int32(1))
+}
+
+func TestMultiplication(t *testing.T) {
+	arith := NewAdapter()
+	answer, err := arith.Addition(5, 2)
+	if err != nil {
+		t.Fatalf("expected: %v, got: %v", nil, err)
+	}
+
+	require.Equal(t, answer, int32(10))
+
+}
+
+func TestDivision(t *testing.T) {
+	arith := NewAdapter()
+	answer, err := arith.Addition(4, 2)
+	if err != nil {
+		t.Fatalf("expected: %v, got: %v", nil, err)
+	}
+
+	require.Equal(t, answer, int32(2))
+
+}
